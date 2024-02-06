2469a273772c49ccb2da155d1fe83f5932ace38e
Matheus Felipe Braga <matheusfbraga10@gmail.com>
2024-02-06 18:28:08 -0300

    JPA Hibernate JAVA
    06-02-24

---
+++ CursoCompletoJAVA/com.udemy.cod3r/src/main/resources/scripts/data-jpa-env.sql
@@ -0,0 +1,5 @@
+
+-- T_ORM_EXAMPLE values
+insert into T_ORM_EXAMPLE (NAME, DESCRIPTION) values ('I', 'One');
+insert into T_ORM_EXAMPLE (NAME, DESCRIPTION) values ('V', 'Five');
+insert into T_ORM_EXAMPLE (NAME, DESCRIPTION) values ('X', 'Ten');
